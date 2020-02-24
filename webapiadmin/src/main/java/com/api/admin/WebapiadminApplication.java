6e47dedf19cbab195d3ac71260d6bfc14b212c16
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-02-24 11:29:23 -0300

    commit inicial

---
+++ webapiadmin/src/main/java/com/api/admin/WebapiadminApplication.java
@@ -0,0 +1,13 @@
+package com.api.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapiadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapiadminApplication.class, args);
+	}
+
+}
---
+++ webapiadmin/src/test/java/com/api/admin/WebapiadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapiadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
